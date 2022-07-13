5fda728cb4be7b69e2668e30a6f4c7c2b6cc15a3
Jacqueline <jcasalia@yahoo.com.br>
2022-07-12 22:51:31 -0300

    atualizando painel do usuario

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const port=3000;
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -39,3 +40,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+app.listen(port, () => {
+  console.log("Estamos rodando em: http://localhost:" + port);
+});
