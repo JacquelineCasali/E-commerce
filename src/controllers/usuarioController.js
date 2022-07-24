7e38619d9475a2370995f6222c6e4d401b315552
Jacqueline <jcasalia@yahoo.com.br>
2022-07-24 09:44:14 -0300

    express1

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const usuarioController={
     index:(req,res)=>{
        return res.render("usuario");
@@ -10,17 +9,4 @@ const usuarioController={
 
 }
 
-=======
-const usuarioController={
-    index:(req,res)=>{
-       return res.render("usuario");
-},
-// deletarUsuario:(req,res)=>{
-//     const {id} = req.params;
-//     res.send("Deletando Usuario com id: " +id)
-// }
-
-}
-
->>>>>>> 7635693b9146c9267b468b80bdf8fc55487cbd78
 module.exports=usuarioController;
