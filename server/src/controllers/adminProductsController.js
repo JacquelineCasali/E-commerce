79225c6aa9f5c599dd0b583ed1e95406b48aebe3
João Pedro <jpdrcbrl@gmail.com>
2022-08-05 13:46:42 -0300

    criando head e footer no partials. AdminProdutos: criando pagina de criar produto, criando rota da pagina

--- server/src/controllers/adminProductsController.js
+++ server/src/controllers/adminProductsController.js
@@ -1,19 +1,32 @@
 const adminProductsController = {
-    adminHome:(req,res)=>{
+    adminHome: (req,res) => {
 
         var produtos = [
-            {
+            {   
+                id: 1,
                 nome: "Camiseta X",
+                categoria: "camiseta",
                 ultimaAlteracao: "05/08/2022",
                 status: "Ativo",
             },
-            {
+            {   
+                id: 2,
                 nome: "Camiseta Y",
+                categoria: "camiseta",
                 ultimaAlteracao: "09/07/2022",
                 status: "Ativo",
             },
-            {
+            {   
+                id: 3,
                 nome: "Camiseta Z",
+                categoria: "camiseta",
+                ultimaAlteracao: "15/08/2021",
+                status: "Inativo",
+            },
+            {   
+                id: 4,
+                nome: "Moletom Z",
+                categoria: "moletom",
                 ultimaAlteracao: "15/08/2021",
                 status: "Inativo",
             }
@@ -21,6 +34,9 @@ const adminProductsController = {
 
         return res.render("adminProdutos", { produtos });
     },
+    adminCriar: (req,res) => {
+        return res.render("adminCriar", { title:"Criar Produto", cssCaminho: "/stylesheets/adminCriar.css" })
+    }
     }
     
     module.exports = adminProductsController;
--- server/src/routes/adminProductsRoute.js
+++ server/src/routes/adminProductsRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const adminProductsController=require("../controllers/adminProductsController");
 
-router.get("/",adminProductsController.adminHome)
+router.get("/", adminProductsController.adminHome)
+router.get("/criar", adminProductsController.adminCriar)
 
 module.exports = router;
