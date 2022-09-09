9ccff9d57f6b88dfe4717611608330b3becdc03b
Jacqueline <103325619+JacquelineCasali@users.noreply.github.com>
2022-09-09 18:28:46 -0300

    Update usuarioRoute.js

--- server/src/routes/usuarioRoute.js
+++ server/src/routes/usuarioRoute.js
@@ -4,47 +4,78 @@ var usuarioController=require("../controllers/usuarioController")
 var meuscreditosController=require("../controllers/meuscreditosController")
 var enderecoController=require("../controllers/enderecoController")
 var cartoesController=require("../controllers/cartoesController")
-
+var meuspedidosController=require("../controllers/meuspedidosController")
+var emailController=require("../controllers/emailController")
+var senhaController=require("../controllers/senhaController")
 
 // get pegar dados, ou seja leitura 
 // post - cadastrar /salvar dados 
 // patch/put -atualizar dados
 // delete - deletar dados 
 router.get("/",usuarioController.index);
+
 router.get("/meuscreditos",meuscreditosController.index);
-router.get("/meuspedidos",meuscreditosController.meuspedidos);
-router.get("/endereco",enderecoController.endereco);
-router.get("/endereco/adicionarendereco",enderecoController.index);
+router.get("/meuspedidos",meuspedidosController.index);
+router.get("/enderecos/",enderecoController.endereco);
+router.get("/enderecos/adicionarendereco",enderecoController.index);
 router.get("/cartoes",cartoesController.cartoes);
 router.get("/cartoes/adicionarcartoes",cartoesController.index);
 
 router.get("/:id",usuarioController.show);
 router.get("/cartoes/:id",cartoesController.show);
-router.get( "/endereco/:id",enderecoController.show);
+router.get("/enderecos/:id",enderecoController.show);
+router.get("/meuspedido/:id",meuspedidosController.show);
+router.get("/meuscreditos/:id",meuscreditosController.show);
+// criar 
+router.get("/enderecos/adicionarendereco",enderecoController.create);
+router.post('/enderecos/adicionarendereco',enderecoController.store)
 
 
-
+router.get("/cartoes/adicionarcartoes",cartoesController.create);
 router.post("/cartoes/adicionarcartoes",cartoesController.store);
-router.post("/endereco/adicionarendereco",enderecoController.store);
-
-
-router.patch("/:id",usuarioController.update);
-router.patch("/cartoes/adicionarcartoes/:id",cartoesController.update);
-router.patch("/endereco/adicionarendereco/:id",enderecoController.update);
 
 
 
-router.put("/:id",usuarioController.update);
-router.put("/cartoes/adicionarcartoes/:id",cartoesController.update);
-router.put("/endereco/adicionarendereco/:id",enderecoController.update);
-
-
-router.delete("/:id",usuarioController.delete);
-router.delete("/cartoes/adicionarcartoes/:id",cartoesController.delete);
-router.delete("/endereco/adicionarendereco/:id",enderecoController.delete);
+// router.patch("/:id",usuarioController.update);
 
 
 
+// editar
+router.get("/cartoes/editarcartoes/:id",cartoesController.edit);
+router.put("/cartoes/editarcartoes/:id",cartoesController.update);
+router.patch("/cartoes/editarcartoes/:id",cartoesController.update);
+
+
+
+router.put("/enderecos/editarendereco/:id",enderecoController.update);
+router.patch("/enderecos/editarendereco/:id",enderecoController.update);
+router.get("/enderecos/editarendereco/:id",enderecoController.edit);
+
+
+
+router.get("/editaremail/:id",emailController.edit);
+router.put("/editaremail/:id",emailController.update);
+router.patch("/editaremail/:id",emailController.update);
+
+
+router.get("/editarsenha/:id",senhaController.edit);
+router.put("/editarsenha/:id",senhaController.update);
+router.patch("/editarsenha/:id",senhaController.update);
+
+
+// router.put("/:id",usuarioController.update);
+
+
+
+// router.delete("/:id",usuarioController.delete);
+router.get("/cartoes/deletarcartao/:id",cartoesController.delete);
+
+router.delete("/cartoes/deletarcartao/:id",cartoesController.destroy);
+
+// deletar
+router.get("/enderecos/deletarenderecos/:id",enderecoController.delete);
+
+router.delete("/enderecos/deletarenderecos/:id",enderecoController.destroy);
 
 
 
